5128d3c64514d173ad0fa02b8bbffb2b0f67d5c3
James <mxtery@gmail.com>
2019-04-10 11:35:27 -0500

    init commit

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 from groupy.client import Client
 import json
 
-def getChats(client):    
-    return client.chats.list()
-
 def main():
-    client = Client.from_token("mzDFlBv3e9JycnZ9LXynMVul2RWgPPWW8SXYa3bC")
+    input_token = input("token: ")
+    client = Client.from_token(input_token)
 
     x = []
     i = 1
+    print("Targets")
+    print("0 - Quit")
     for chat in client.chats.list():
         print(str(i) + ' - ', end="")
         print( chat.other_user['name'])
         x.append(chat)
         i+=1
 
-    chats = getChats(client)
-
+    message = "fck"
+    i = 1
     while(True):
-        input_target = input("who do you want to talk to?")
+        '''
+        input_target = input("target: ")
+
+        if input_target == '0':
+            break
+        
         input_message = input("message: ")
-        
-        message = x[int(input_target)-1].post(text = input_message)
-        
+        '''
+        #x[int(input_target)-1].post(text = input_message)
+        x[int(2)-1].post(text = "Yo where you at?")
+        print("message sent to: ", end="")
+        print(x[1])
+        print("#: ")
+        print(i)
+        i+=1
+        '''
+        message +="k"
+        '''
+
 
 
 
